35a6d984e72212687d2d79b2189f2d2ce448ece1
krc92001 <76540945+krc92001@users.noreply.github.com>
2022-06-01 11:39:58 -0400

    Initial Commit

---
+++ rename.py
@@ -0,0 +1,20 @@
+import sys
+#takes in directory as argument
+
+
+
+def create_file_list(directory):
+  """Grabs list of files from the specified directory and finds the files containing illegal  names"""
+  
+def main():
+  pass
+
+if __name__ == "__main__":
+    main()
+  
+
+
+
+
+
+
